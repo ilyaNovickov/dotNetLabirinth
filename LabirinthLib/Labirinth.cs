53e54b7df84d43ba90da921f158e68a8a41f800c
Ilya Novikov <ilyanovickov@mail.ru>
2023-09-20 20:16:36 +0300

    Много чего добавлено в лабиринт

--- LabirinthLib/Labirinth.cs
+++ LabirinthLib/Labirinth.cs
@@ -8,5 +8,69 @@ namespace LabirinthLib
 {
     public class Labirinth
     {
+        float countofEmptySpace = 0.4f;
+        Size size;
+        Point firstIn;
+        Point secIn;
+        Point exit;
+        int[,] lab;
+
+        public Labirinth() : this(new Size(10, 10))
+        {
+
+        }
+
+        public Labirinth(int width, int height) : this(new Size(width, height))
+        {
+
+        }
+
+        public Labirinth(Size size)
+        {
+            this.Size = size;
+        }
+
+        private int this[Point point]
+        {
+            get => lab[point.X, point.Y];
+            set => lab[point.X, point.Y] = value;
+        }
+
+        private int this[int x, int y]
+        {
+            get => lab[x, y];
+            set => lab[x, y] = value;
+        }
+
+        public float EmptySpace
+        {
+            get => countofEmptySpace;
+            set
+            {
+                if (0 < value && value < 1f)
+                {
+                    countofEmptySpace = value;
+                }
+                else
+                {
+                    countofEmptySpace = 0.4f;
+                }
+            }
+        }
+
+        public Size Size
+        {
+            get => size;
+            set
+            {
+                if (value.Width < 3 && value.Height < 3)
+                    return;
+                lab = new int[value.Width, value.Height];
+            }
+        }
+
+        public Point FirstIn => firstIn;
+        public Point SecondIn => secIn;
+        public Point Exit => exit;
     }
 }
