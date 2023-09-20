46d65671fb295b9b846ad40ef77e2e359db05b79
Ilya Novikov <ilyanovickov@mail.ru>
2023-09-20 19:52:38 +0300

    Изменена структура Point

--- LabirinthLib/Point.cs
+++ LabirinthLib/Point.cs
@@ -8,5 +8,15 @@ namespace LabirinthLib
 {
     public struct Point
     {
+        private int x, y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X { get { return x; } set {  x = value; } }
+        public int Y { get { return y; } set { y = value; } }
     }
 }
