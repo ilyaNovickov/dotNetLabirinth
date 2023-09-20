b1780115b406659810f837adac411b2182f1a752
Ilya Novikov <ilyanovickov@mail.ru>
2023-09-20 20:05:37 +0300

    Добавлена структуктура размера

---
+++ LabirinthLib/Size.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabirinthLib
+{
+    public struct Size
+    {
+        int width, height;
+        public Size(int width, int height)
+        {
+            if (width < 0 || height < 0)
+                throw new Exception("Отрицательный размер");
+            this.width = width;
+            this.height = height;
+        }
+        public int Width
+        {
+            get => width;
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Отрицательный размер");
+                width = value;
+            }
+        }
+        public int Height
+        {
+            get => height;
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Отрицательный размер");
+                height = value;
+            }
+        }
+    }
+}
