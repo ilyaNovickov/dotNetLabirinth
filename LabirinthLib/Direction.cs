33161bc56678a1ba9dffc379ea6f7ffcef228914
Ilya Novikov <ilyanovickov@mail.ru>
2023-09-27 20:49:57 +0300

    Добавлены методы расширения

--- LabirinthLib/Direction.cs
+++ LabirinthLib/Direction.cs
@@ -14,4 +14,25 @@ namespace LabirinthLib
         Left = -2, 
         Right = 2
     }
+
+    public static class DirectionExtandentClass
+    {
+        public static void OffsetPoint(this Point point, params Direction[] dirs)
+        {
+            foreach (Direction direction in dirs)
+            {
+                switch (direction)
+                {
+                    case Direction.Up:
+                    case Direction.Down:
+                        point.Y += (int)direction;
+                        break;
+                    case Direction.Left:
+                    case Direction.Right:
+                        point.X += (int)direction / 2;
+                        break;
+                }
+            }
+        }
+    }
 }
