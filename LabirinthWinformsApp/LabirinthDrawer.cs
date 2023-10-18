87863f23c4d2032e0f7186f806581e786995f662
tverstu <tverstu@gmail.com>
2023-10-18 16:24:36 +0300

    Добавлен метод тестовый для рисования лабиринта

---
+++ LabirinthWinformsApp/LabirinthDrawer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using LabirinthLib;
+using LabirinthLib.Structs;
+
+namespace LabirinthWinformsApp
+{
+    public static class LabirinthDrawer
+    {
+        public static void DrawLabirinth(this Labirinth lab, Graphics g)
+        {
+            Pen pen = new Pen(Color.Black, 2);
+            SolidBrush brush = new SolidBrush(Color.White);
+
+            g.DrawRectangle(pen, 0, 0, lab.Width, lab.Height);
+        }
+    }
+}
