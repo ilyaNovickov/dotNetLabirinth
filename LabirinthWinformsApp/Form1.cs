81ae499bc4de2037e1be6016a3e2bee65b3cddcf
tverstu <tverstu@gmail.com>
2023-10-18 16:27:38 +0300

    Рисование либиринта на форме тест1

--- LabirinthWinformsApp/Form1.cs
+++ LabirinthWinformsApp/Form1.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LabirinthLib;
+using LabirinthLib.Structs;
 
 namespace LabirinthWinformsApp
 {
     public partial class Form1 : Form
     {
+        Labirinth lab = new Labirinth(10); 
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(lab.Width, lab.Height);
+            lab.DrawLabirinth(Graphics.FromImage(bitmap));
+            pictureBox1.Image = bitmap;
         }
     }
 }
